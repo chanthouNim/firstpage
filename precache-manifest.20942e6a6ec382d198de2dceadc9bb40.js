36e53b23938f9ef3cb3c5f6dc1209648236be418
Chanthou Nim <chanthou@web-essentials.asia>
2019-05-23 01:37:15 +0700

    [TASK] Add deploy file
    refs REA-004

---
+++ precache-manifest.20942e6a6ec382d198de2dceadc9bb40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0638f8dcff618971a1bd65e3800fed14",
+    "url": "/firstpage/index.html"
+  },
+  {
+    "revision": "ef284d884ac9effe4be9",
+    "url": "/firstpage/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/firstpage/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "ef284d884ac9effe4be9",
+    "url": "/firstpage/static/js/main.5563ac66.chunk.js"
+  },
+  {
+    "revision": "e97af7cd3274b2fb50a4",
+    "url": "/firstpage/static/js/runtime~main.fe6dbb05.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/firstpage/static/media/logo.5d5d9eef.svg"
+  }
+]);
